da84a30f697045a59c96672c672915542889acf3
Игорь Тихоненко <monzar1993@outlook.com>
2020-03-04 17:06:39 +0200

    js code fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-var numbers = [ 254, 115, 78, 25, 91, 45, 37 ]
+var numbers = [254, 115, 78, 25, 91, 45, 37]
 
-for ( var a = 0; a < numbers.length; a++) {
-
-    
-    if ( numbers[a] >= 50 ) {
-        console.log (numbers[a])
+for (var a = 0; a < numbers.length; a++) {
+    if (numbers[a] >= 50) {
+        console.log(numbers[a])
     } else {
         // console.log ("Less than 50")
     }
