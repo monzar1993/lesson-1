17875853360c45c715d7bd85cd7b8adcbf7f7c3f
Игорь Тихоненко <monzar1993@outlook.com>
2020-03-05 13:55:52 +0200

    if - else transformed in ES6 code style

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var numbers = [254, 115, 78, 25, 91, 45, 37]
 
 for (var a = 0; a < numbers.length; a++) {
-    if (numbers[a] >= 50) {
-        console.log(numbers[a])
-    } else {
-        // console.log ("Less than 50")
-    }
+    // if (numbers[a] >= 50) {
+    //     console.log(numbers[a])
+    // } else {}
+    var filter = numbers[a] >= 50 ? console.log(numbers[a]) : false
 }
